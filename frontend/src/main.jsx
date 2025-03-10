d0c99ebec654e51f674a5ffa25335f488b06d8e0
Petko Todorov <p.todorov1015@gmail.com>
2025-03-10 19:24:06 +0200

    add google auth provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AuthProvider } from './context/AuthContext';
+
+const clientId = "1054962942187-uge626asu54djmb3n7c1nnisi86janqf.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
-    </StrictMode>,
+        <GoogleOAuthProvider clientId={clientId}>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </GoogleOAuthProvider>
+    </StrictMode>
 )
