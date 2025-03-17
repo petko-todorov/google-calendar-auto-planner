82bd2a65a4fab0299be7a78f9a721317e166794a
Petko Todorov <p.todorov1015@gmail.com>
2025-03-17 19:00:59 +0200

    small view change

--- backend/google_auth/urls.py
+++ backend/google_auth/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import GoogleLoginView, UserInfoView, LogoutView
 
 urlpatterns = [
     path('user-info/', UserInfoView.as_view(), name='user_info'),
-    path('login/', GoogleLoginView.as_view(), name='google_login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', GoogleLoginView.as_view(), name='login'),
 ]
