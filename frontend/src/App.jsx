ddde142f51b5028de0a07ce1b3a717fb23a83786
Petko Todorov <p.todorov1015@gmail.com>
2025-03-24 10:23:20 +0200

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,40 @@
+import './App.css';
 import { useAuth } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
+import googleLoading from './assets/google-loading.gif'
+import ProfileImageMenu from './components/ProfileImageMenu';
+import Header from './components/Header';
 
 function App() {
-    const { isAuthenticated, userData, error, login, logout } = useAuth();
+    const { isAuthenticated, userData, error, login, logout, loading } = useAuth();
 
     return (
-        <div className="app">
-            <div className="container">
-                <h1>Google Auth Test</h1>
-                {error && <div className="error-message">{error}</div>}
+        <>
+            <Header />
+            {loading && (
+                <>
+                    <div className='text-center pt-10 flex justify-center'>
+                        <img src={googleLoading} alt="" />
+                    </div>
+                </>
+            )}
 
-                {!isAuthenticated ? (
-                    <LoginPage onLoginSuccess={login} />
-                ) : (
-                    <ProfilePage userData={userData} onLogout={logout} />
-                )}
-            </div>
-        </div>
+            {!loading && (
+                <div>
+                    {/* {error && <div className="error-message">{error}</div>} */}
+
+                    {!isAuthenticated ? (
+                        <LoginPage onLoginSuccess={login} />
+                    ) : (
+                        <>
+                            {/* <ProfileImageMenu userData={userData} /> */}
+                            <ProfilePage userData={userData} onLogout={logout} />
+                        </>
+                    )}
+                </div>
+            )}
+        </>
     );
 }
 
