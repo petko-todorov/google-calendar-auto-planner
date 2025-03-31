55d3ea8491a5e7bcb9622cad722194814a2e38e9
Petko Todorov <p.todorov1015@gmail.com>
2025-03-31 14:33:26 +0300

    small changes

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -4,27 +4,25 @@ const ProfilePage = ({ userData, onLogout }) => {
     return (
         <>
             <div className="w-4/5 mx-auto border-2">
-                <div className="">
-                    <div className="mt-4 text-center">
-                        <h2 className="text-2xl font-semibold">
-                            {userData?.first_name} {userData?.last_name}
-                        </h2>
-                        {/* <div className="">
+                <div className="mt-4 text-center">
+                    <h2 className="text-2xl font-semibold">
+                        {userData?.first_name} {userData?.last_name}
+                    </h2>
+                    {/* <div className="">
                             <p>{userData?.first_name} {userData?.last_name}</p>
                             <p>{userData?.email}</p>
                             <p>{userData?.google_id}</p>
                         </div> */}
 
-                        {/* <button
+                    {/* <button
                             onClick={onLogout}
                         >
                             Logout
                         </button> */}
-                    </div>
+                </div>
 
-                    <div className="mt-6 w-full">
-                        <CalendarEvents />
-                    </div>
+                <div className="mt-6 w-full">
+                    <CalendarEvents />
                 </div>
             </div>
         </>
