1e5b34e1593ab35e40ff55396236228531ead755
Petko Todorov <p.todorov1015@gmail.com>
2025-04-07 21:52:28 +0300

    Initial commit

---
+++ frontend/src/components/DeleteConfirmationDialog.jsx
@@ -0,0 +1,34 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button, Typography } from '@mui/material';
+
+const DeleteConfirmationDialog = ({ open, eventTitle, onCancel, onConfirm }) => {
+    return (
+        <Dialog open={open} onClose={onCancel} maxWidth="xs" fullWidth>
+            <DialogTitle>
+                Confirm Delete
+            </DialogTitle>
+            <DialogContent>
+                <Typography>
+                    Are you sure you want to delete "{eventTitle}"?
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={onCancel}
+                    color="primary"
+                >
+                    Cancel
+                </Button>
+                <Button
+                    onClick={onConfirm}
+                    color="error"
+                    variant="contained"
+                    autoFocus
+                >
+                    Delete
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default DeleteConfirmationDialog;
