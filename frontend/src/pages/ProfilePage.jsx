1332831d3b6614fcdc9346ee7af84a9f3e3e8446
Petko Todorov <p.todorov1015@gmail.com>
2025-04-11 13:20:52 +0300

    Minor changes

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -3,22 +3,11 @@ import CalendarEvents from "../components/CalendarEvents";
 const ProfilePage = ({ userData, onLogout }) => {
     return (
         <>
-            <div className="w-11/12 mx-auto border-2">
+            <div className="w-11/12 mx-auto border-2 backdrop-blur-sm">
                 <div className="mt-4 text-center">
                     <h2 className="text-2xl font-semibold">
                         {userData?.first_name} {userData?.last_name}
                     </h2>
-                    {/* <div className="">
-                            <p>{userData?.first_name} {userData?.last_name}</p>
-                            <p>{userData?.email}</p>
-                            <p>{userData?.google_id}</p>
-                        </div> */}
-
-                    {/* <button
-                            onClick={onLogout}
-                        >
-                            Logout
-                        </button> */}
                 </div>
 
                 <div className="mt-6 w-full">
