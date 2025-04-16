590b9d205493b3113070b7cf27ff3433972b6aa0
Petko Todorov <p.todorov1015@gmail.com>
2025-04-16 14:17:32 +0300

    Add format date and small css fixes

--- frontend/src/components/EventDetails.jsx
+++ frontend/src/components/EventDetails.jsx
@@ -1,6 +1,14 @@
 import { Box, Button, Typography } from '@mui/material';
 
 const EventDetails = ({ event }) => {
+    const formatDate = (date) => {
+        if (!date) return '';
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}-${month}-${year}`;
+    }
+
     const formatTime = (date) => {
         if (!date) return '';
         const hours = date.getHours().toString().padStart(2, '0');
@@ -10,22 +18,44 @@ const EventDetails = ({ event }) => {
 
     return (
         <>
-            <Typography variant="h5" component="div" align="center" fontWeight="bold">
+            <Typography
+                variant="h5"
+                component="div"
+                align="center"
+                fontWeight="bold"
+                gutterBottom
+            >
                 {event.title}
             </Typography>
             <Box mb={2}>
-                <Typography variant="subtitle1" gutterBottom>
+                <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                >
+                    <strong>Date:</strong> {formatDate(event.start)}
+                </Typography>
+
+                <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                >
                     <strong>Time:</strong> {formatTime(event.start)} - {formatTime(event.end)}
                 </Typography>
 
                 {event.description && (
-                    <Typography variant="body1" gutterBottom>
+                    <Typography
+                        variant="body1"
+                        gutterBottom
+                    >
                         <strong>Description:</strong> {event.description}
                     </Typography>
                 )}
 
                 {event.location && (
-                    <Typography variant="body1" gutterBottom>
+                    <Typography
+                        variant="body1"
+                        gutterBottom
+                    >
                         <strong>Location:</strong> {event.location}
                     </Typography>
                 )}
