ba954c0cc7550d56be0c7829822ce782710c0b44
Petko Todorov <p.todorov1015@gmail.com>
2025-04-16 13:52:58 +0300

    Small css change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <h1 className='text-4xl font-bold mt-10 mb-5 text-center text-zinc-900'>
+        <h1 className='text-4xl font-bold mt-6 mb-5 text-center text-zinc-900'>
             Google Calendar App
         </h1>
     );
